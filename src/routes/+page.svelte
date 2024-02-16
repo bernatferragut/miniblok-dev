187e7059f5e1c103b2f1aef64ca9f941e4e5f50e
Bernat Ferragut <elbernat@gmail.com>
2024-02-16 10:56:42 -0500

    header footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,5 @@
 </section>
 
 <style>
-	section {
-		display: flex;
-		justify-content: center;
-		margin-top: 25vh;
-	}
-	code {
-		color: lightgrey;
-		text-transform: uppercase;
-		font-size: 8em;
-		font-weight: 200;
-		margin: auto;
-	}
 
-	/* @media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	} */
 </style>
