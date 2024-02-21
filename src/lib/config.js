d52a32d16de73177674dce54d0578f3a74d49fb5
Bernat Ferragut <elbernat@gmail.com>
2024-02-21 11:08:34 -0500

    build created

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
 export const title = 'M1N1BL0K ❒'
 export const description = 'Blog made of Bloks'
 import { dev } from '$app/environment'
-export const url = dev ? 'http://localhost:5173' : 'http://miniblok.xyz'
+// export const url = dev ? 'http://localhost:5173' : 'http://miniblok.xyz'
